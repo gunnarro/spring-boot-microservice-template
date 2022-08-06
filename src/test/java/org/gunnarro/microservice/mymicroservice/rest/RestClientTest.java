49a80c0d3b5d05b3044719dd1e6d2bbd094123e5
gunnarro <gunnar_ronneberg@yahoo.no>
2022-08-06 19:50:16 +0000

    github action updated microservice template

--- src/test/java/org/gunnarro/microservice/mymicroservice/rest/RestClientTest.java
+++ src/test/java/org/gunnarro/microservice/mymicroservice/rest/RestClientTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 class RestClientTest extends DefaultTestConfig {
     @BeforeEach
