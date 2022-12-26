479e33cfd2ff00f5890152110bb6078ccb6b2422
gunnarro <gunnar_ronneberg@yahoo.no>
2022-12-26 16:13:47 +0000

    github action commit generated code

--- src/main/java/org/gunnarro/microservice/mymicroservice/endpoint/RestServiceController.java
+++ src/main/java/org/gunnarro/microservice/mymicroservice/endpoint/RestServiceController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * TODO Remove or Refactor. This class only serves as an example!
