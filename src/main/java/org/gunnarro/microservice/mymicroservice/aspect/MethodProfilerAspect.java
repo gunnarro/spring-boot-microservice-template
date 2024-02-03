f2208a8baf6bf3ee2e273fbaeae187ee28055bef
gunnarro <gunnar_ronneberg@yahoo.no>
2024-02-03 13:27:34 +0000

    github action commit generated code

--- src/main/java/org/gunnarro/microservice/mymicroservice/aspect/MethodProfilerAspect.java
+++ src/main/java/org/gunnarro/microservice/mymicroservice/aspect/MethodProfilerAspect.java
@@ -9,35 +9,43 @@ import org.springframework.util.StopWatch;
 import org.springframework.util.StopWatch.TaskInfo;
 import org.springframework.web.client.HttpClientErrorException;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.util.StopWatch.TaskInfo;
+import org.springframework.web.client.HttpClientErrorException;
+
 /**
  * TODO Remove unused profiling paths
- * 
  * Aspect used for performance logging.
- * 
- * @see https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#aop
- * 
+ *
+ * @see <a href="https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#aop">spring doc core</a>
+ *
  */
 @Aspect
 @Component
 @Slf4j
 public class MethodProfilerAspect {
 
-    @Around("execution(* org.gunnarro.microservice.mymicroservice.adapter.impl.*.*(..))")
+    @Around("execution(* org.gunnarro.microservice.todoservice.adapter.impl.*.*(..))")
     public Object profileAdapters(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         return profileMethodeCall(proceedingJoinPoint);
     }
 
-    @Around("execution(* org.gunnarro.microservice.mymicroservice.rest.RestClient.*(..))")
+    @Around("execution(* org.gunnarro.microservice.todoservice.rest.RestClient.*(..))")
     public Object profileRestClient(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         return profileMethodeCall(proceedingJoinPoint);
     }
 
-    @Around("execution(* org.gunnarro.microservice.mymicroservice.service.impl.*.*(..))")
+    @Around("execution(* org.gunnarro.microservice.todoservice.service.impl.*.*(..))")
     public Object profileServices(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         return profileMethodeCall(proceedingJoinPoint);
     }
 
-    @Around("execution(* org.gunnarro.microservice.mymicroservice.endpoint.*.*(..))")
+    @Around("execution(* org.gunnarro.microservice.todoservice.endpoint.*.*(..))")
     public Object profileEndpoint(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         return profileMethodeCall(proceedingJoinPoint);
     }
@@ -54,7 +62,7 @@ public class MethodProfilerAspect {
             throw exception;
         } finally {
             stopWatch.stop();
-            TaskInfo taskInfo = stopWatch.getLastTaskInfo();
+            TaskInfo taskInfo = stopWatch.lastTaskInfo();
             String taskName = taskInfo.getTaskName().replace("execution(", "").replace("(..))", "");
             String msg = String.format("%s", exceptionMsg != null ? "exception=\"" + exceptionMsg + "\"" : "");
             log.info("method=\"{}\" time=\"{} ms\" {}", taskName, taskInfo.getTimeMillis(), msg);
