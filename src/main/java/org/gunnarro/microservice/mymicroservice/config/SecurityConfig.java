9256b6717d56ad046a87d95854e2d5ad28d3edaf
gunnarro <gunnar_ronneberg@yahoo.no>
2024-02-03 02:16:06 +0000

    github action commit generated code

--- src/main/java/org/gunnarro/microservice/mymicroservice/config/SecurityConfig.java
+++ src/main/java/org/gunnarro/microservice/mymicroservice/config/SecurityConfig.java
@@ -2,19 +2,23 @@ package org.gunnarro.microservice.mymicroservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * Security configuration for Rest API and endpoints
- * 
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     /**
@@ -26,14 +30,27 @@ public class SecurityConfig {
      * <p>
      * Both CORS and CSRF are activated as default bt Spring Security 4.x and higher.
      */
-    @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                        .antMatchers("/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/actuator/**")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        (requests) -> requests
+                                .requestMatchers(HttpMethod.GET, "/api-docs/*", "/swagger-resources/**", "/swagger-ui.html", "/actuator/**").permitAll()
+                                .anyRequest().authenticated()
+                );
         return http.build();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("/**"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+        configuration.setExposedHeaders(List.of("X-Get-Header"));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
